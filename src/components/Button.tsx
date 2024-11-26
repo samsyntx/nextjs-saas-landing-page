6df82d00abfe2bff93266d2d59a18ef35d227934
samsyntx <samaroraytb@gmail.com>
2024-11-26 17:04:52 +0530

    Completed Hero Section

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from "react";
+import React, { ButtonHTMLAttributes, HTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 const classess = cva("border h-12 rounded-full px-6 font-medium", {
@@ -7,21 +7,22 @@ const classess = cva("border h-12 rounded-full px-6 font-medium", {
             primary: "bg-lime-400 text-neutral-950 border-line-400",
             secondary: "border-white text-white bg-transparent",
         },
+        size: {
+            sm: "h-10",
+        },
     },
 });
 
 const Button = (
     props: {
-        type?: "submit" | "reset" | "button";
         variant: "primary" | "secondary";
-    } & HTMLAttributes<HTMLButtonElement>
+        size?: "sm";
+    } & ButtonHTMLAttributes<HTMLButtonElement>
 ) => {
-    const { variant, className, type = "button", children } = props;
+    const { variant, className, size, ...rest } = props;
 
     return (
-        <button type={type} className={classess({ variant, className })}>
-            {children}
-        </button>
+        <button className={classess({ variant, className, size })} {...rest} />
     );
 };
 
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,54 @@
+import Button from "@/components/Button";
+import designExample1 from "@/assets/images/design-example-1.png";
+import designExample2 from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
+
 export default function Hero() {
-    return <div>Hero</div>;
+    return (
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative ">
+                <div className="absolute -left-32 top-16 hidden lg:block">
+                    <Image src={designExample1} alt="design example 1" />
+                </div>
+                <div className="absolute -right-64 -top-16 hidden lg:block">
+                    <Image src={designExample2} alt="design example 2" />
+                </div>
+                <div className="absolute top-96 left-56 hidden lg:block">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute -top-4 right-80 hidden lg:block">
+                    <Pointer color="red" name="Brew" />
+                </div>
+                <div className="flex justify-center">
+                    <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
+                        ✨ $7.5M eed round raised
+                    </div>
+                </div>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6 ">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+                    Design tool shouldn't slow you down. Create, prototype, and
+                    test with ease.
+                </p>
+                <form className="mx-auto flex border border-white/50 rounded-full p-2 mt-8 max-w-lg">
+                    <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="bg-transparent px-4 flex-1"
+                    />
+                    <Button
+                        size="sm"
+                        className="whitespace-nowrap"
+                        type="submit"
+                        variant="primary"
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+                
+            </div>
+        </section>
+    );
 }
