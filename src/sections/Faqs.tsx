91019077c8ecd564cab529a93f47042c38c6eeff
samsyntx <samaroraytb@gmail.com>
2024-12-03 16:47:44 +0530

    Completed Development of CalltoAction & Footer

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,3 +1,7 @@
+import Tag from "@/components/Tag";
+import { Plus } from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
 const faqs = [
     {
         question: "How is Layers different from other design tools?",
@@ -22,5 +26,48 @@ const faqs = [
 ];
 
 export default function Faqs() {
-    return <div>Faqs</div>;
+    const selectedIndex = 0;
+
+    return (
+        <section className="py-24 ">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag>Faqs</Tag>
+                </div>
+                <h2 className="text-6xl font-medium mt-6 text-center max-w-xl mx-auto">
+                    Questions? We've got{" "}
+                    <span className="text-lime-400">answers</span>
+                </h2>
+
+                <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
+                    {faqs.map((faq, faqIndex) => (
+                        <div
+                            key={faq.question}
+                            className="bg-neutral-900 rounded-2xl border border-white/10 p-6 "
+                        >
+                            <div className="flex justify-between items-center">
+                                <h3 className="font-medium ">{faq.question}</h3>
+                                <Plus
+                                    size={30}
+                                    className={twMerge(
+                                        "feather feather-plus text-lime-400 flex-shrink-0",
+                                        selectedIndex === faqIndex &&
+                                            "rotate-45"
+                                    )}
+                                />
+                            </div>
+                            <div
+                                className={twMerge(
+                                    "mt-6",
+                                    selectedIndex !== faqIndex && "hidden"
+                                )}
+                            >
+                                <p className="text-white/50">{faq.answer}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import siteLogo from "@/assets/images/logo.svg";
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,27 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className="py-16">
+            <div className="container">
+                <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-6">
+                    <div>
+                        <Image src={siteLogo} alt="Layers Logo" />
+                    </div>
+                    <div>
+                        <nav className="flex gap-6">
+                            {footerLinks.map((link) => (
+                                <a
+                                    href={link.href}
+                                    className="text-white/50 text-sm "
+                                >
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
