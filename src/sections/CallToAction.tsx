323c71db349983972e24fd6756c7b6a5396c39bf
samsyntx <samaroraytb@gmail.com>
2024-12-07 21:24:18 +0530

    Completed Dynamic Animation in callToAction

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,28 +1,48 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { AnimationPlaybackControls, motion, useAnimate } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function CallToAction() {
+    const animation = useRef<AnimationPlaybackControls>();
+    const [scope, animate] = useAnimate();
+
+    const [slowDownAnimation, setSlowDownAnimation] = useState(false);
+
+    useEffect(() => {
+        animation.current = animate(
+            scope.current,
+            { x: "-50%" },
+            { duration: 30, ease: "linear", repeat: Infinity }
+        );
+    }, []);
+
+    useEffect(() => {
+        if (animation.current) {
+            if (slowDownAnimation) {
+                animation.current.speed = 0.5;
+            } else {
+                animation.current.speed = 1;
+            }
+        }
+    }, [slowDownAnimation]);
+
     return (
         <section className="py-24">
             <div className="overflow-x-clip p-4 flex">
                 <motion.div
-                    animate={{
-                        x: "-50%",
-                    }}
-                    transition={{
-                        duration: 30,
-                        ease: "linear",
-                        repeat: Infinity,
-                    }}
+                    ref={scope}
                     className="flex flex-none gap-16 pr-16 text-7xl md:text-8xl font-medium"
+                    onMouseEnter={() => setSlowDownAnimation(true)}
+                    onMouseLeave={() => setSlowDownAnimation(false)}
                 >
                     {Array.from({ length: 10 }).map((_, index) => (
                         <div key={index} className="flex items-center gap-16">
                             <span className="text-lime-400 text-7xl ">
                                 &#10038;
                             </span>
-                            <span>Try it for free</span>
+                            <span className={twMerge(slowDownAnimation && "text-lime-400")}>Try it for free</span>
                         </div>
                     ))}
                 </motion.div>
